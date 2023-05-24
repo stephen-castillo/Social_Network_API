3900a62eb5c9199558c8a9da97c2281ab5f9717d
stephen-castillo <122939431+stephen-castillo@users.noreply.github.com>
2023-05-23 21:09:11 -0500

    Created User model based on template, updated models index.js with correct path

--- models/User.js
+++ models/User.js
@@ -1,21 +1,32 @@
-/* User:
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
 
-username
+// Schema to create User model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      maxlength: 50,
+    },
+    thoughts: [thoughtSchema],
+    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
-String
-Unique
-Required
-Trimmed
+const User = model('User', userSchema);
 
-email
-
-String
-Required
-Unique
-Must match a valid email address (look into Mongoose's matching validation)
-thoughts
-
-Array of _id values referencing the Thought model
-friends
-
-Array of _id values referencing the User model (self-reference) */
+module.exports = User;
